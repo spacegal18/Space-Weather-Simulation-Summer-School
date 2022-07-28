ea0fc1bf333a4af855db66b19aa8e89e83d4b402
Neha Srivastava <ns1202@wildcats.unh.edu>
2022-07-28 11:24:25 -0400

    pollutant

--- day_8/Convection/PollutantTransport1D.py
+++ day_8/Convection/PollutantTransport1D.py
@@ -30,7 +30,7 @@ plt.close()
 import matplotlib.animation as animation
 
 "Flow parameters"
-nu = 0.01
+nu = 0
 c = -2
 u0 = 0
 
@@ -45,7 +45,7 @@ xN = np.concatenate(([x[0]-Dx],x))
 
 
 "Time parameters"
-dt = 1/50
+dt = 1/100
 time = np.arange(0,3+dt,dt)
 nt = np.size(time)
 
@@ -144,6 +144,135 @@ if nu>0:
 "CFL number"
 CFL = np.abs(c*dt/Dx)
 print("CFL number CFL=%g\n" % CFL);
+
+
+
+#%%
+"""Without limiter"""
+#!/usr/bin/env python
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+%matplotlib qt
+plt.close()
+import matplotlib.animation as animation
+
+"Flow parameters"
+nu = 0
+c = -2
+u0 = 0
+
+"Scheme parameters"
+beta = 0
+
+"Number of points"
+N = 32
+Dx = 1/N
+x = np.linspace(0,1,N+1)
+xN = np.concatenate(([x[0]-Dx],x))
+
+
+"Time parameters"
+dt = 1/10
+time = np.arange(0,3+dt,dt)
+nt = np.size(time)
+
+"Initialize solution variable"
+U = np.zeros((N+2,nt))
+
+
+for it in range(nt-1):
+
+    "System matrix and RHS term"
+    "Diffusion term"
+    Diff = nu*(1/Dx**2)*(2*np.diag(np.ones(N+2)) - np.diag(np.ones(N+1),-1) - np.diag(np.ones(N+1),1))
+
+    "Advection term:"
+        
+    "Sensor"
+    U0 = U[:,it]
+   
+    
+    # "Limiter"
+    # if beta>0:
+    #     phi = np.minimum(np.minimum(beta*r,1),np.minimum(r,beta))
+    #     phi = np.maximum(0,phi)
+    # else:
+    #     phi = 2*r/(r**2 + 1)
+    
+    
+    order =2
+    "Upwind scheme"
+    cp = np.max([c,0])
+    cm = np.min([c,0])
+    if order<2:
+        Advp = cp*(np.diag(np.ones(N+2)) - np.diag(np.ones(N+1),-1))
+        Advm = cm*(np.diag(np.ones(N+2)) - np.diag(np.ones(N+1),1))
+    else:
+        "Centered differences"
+        Advp = -0.5*c*(1.5*np.diag(np.ones(N+2)) - 2*np.diag(np.ones(N+1),-1) + 0.5*np.diag(np.ones(N),-2))
+        Advm = -0.5*c*(1.5*np.diag(np.ones(N+2)) - 2*np.diag(np.ones(N+1),1) + 0.5*np.diag(np.ones(N),2))
+        
+    
+    
+    Ahigh = Advp-Advm    
+    Adv = (1/Dx)*(Ahigh + Alow)
+    A = Diff + Adv
+    
+    "Source term"
+    sine = np.sin(2*pi*time[it+1])
+    sineplus = 0.5*(sine + np.abs(sine))
+    F = 100*np.exp(-((x-0.8)/0.01)**2)*sineplus
+    
+    "Temporal terms"
+    A = A + (1/dt)*np.diag(np.ones(N+2))
+    F = F + U0/dt
+
+    "Boundary condition at x=0"
+    A[0,:] = (1/Dx)*np.concatenate(([1.5, -2, 0.5],np.zeros(N)))
+    F[0] = 0
+
+    "Boundary condition at x=1"
+    A[N+1,:] = (1/Dx**2)*np.concatenate((np.zeros(N) +  ))
+    F[N+1] = u0
+
+
+    "Solution of the linear system AU=F"
+    u = np.linalg.solve(A,F)
+    U[:,it+1] = u
+    u = u[0:N+1]
+
+
+"Animation of the results"
+fig = plt.figure()
+ax = plt.axes(xlim =(0, 1),ylim =(u0-1e-2,u0+0.5)) 
+myAnimation, = ax.plot([], [],':ob',linewidth=2)
+plt.grid()
+plt.xlabel("x",fontsize=16)
+plt.ylabel("u",fontsize=16)
+
+def animate(i):
+    
+    u = U[0:N+1,i]
+    plt.plot(x,u)
+    myAnimation.set_data(x, u)
+    return myAnimation,
+
+anim = animation.FuncAnimation(fig,animate,frames=range(1,nt),blit=True,repeat=False)
+
+
+if nu>0:
+    "Peclet number"
+    P = np.abs(c*Dx/nu)
+    print("Pe number Pe=%g\n" % P);
+
+"CFL number"
+CFL = np.abs(c*dt/Dx)
+print("CFL number CFL=%g\n" % CFL);
+
 
 
 
